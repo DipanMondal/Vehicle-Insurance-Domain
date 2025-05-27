07cfbf1b6fdf76704b4ff550d95f14b95d643131
Dipan31Mondal <idipan2003@gmail.com>
2025-05-27 20:23:50 +0530

    src/data_access/vehicle_data.py Modified

--- src/data_access/vehicle_data.py
+++ src/data_access/vehicle_data.py
@@ -48,8 +48,6 @@ class VehicleData:
             print("Fetching data from mongoDB")
             df = pd.DataFrame(list(collection.find()))
             print(f"Data fecthed with len: {len(df)}")
-            if "id" in df.columns.to_list():
-                df = df.drop(columns=["id"], axis=1)
             df.replace({"na":np.nan},inplace=True)
             return df
 
