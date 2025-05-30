49708e25c06bf1c0cec46f3ff1902072c4e0ecba
Dipan31Mondal <idipan2003@gmail.com>
2025-05-30 23:01:19 +0530

    model evaluator and model pusher component is complete and functional

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -55,3 +55,14 @@ class ModelTrainerConfig:
     _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
     _criterion = MIN_SAMPLES_SPLIT_CRITERION
     _random_state = MIN_SAMPLES_SPLIT_RANDOM_STATE
+    
+
+@dataclass
+class ModelEvaluationConfig:
+    changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    ggl_model_path: str = MODEL_FILE_NAME
+    
+    
+@dataclass
+class ModelPusherConfig:
+    ggl_model_path: str = MODEL_FILE_NAME
